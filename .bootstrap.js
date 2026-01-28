0f1f6106669c67851c1f856614d6240db17168d2
Vibesdk <vibesdk-bot@cloudflare.com>
2026-01-28 22:38:19 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,10 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add jspdf@^2.5.1",
+    "bun add jspdf-autotable@^3.8.2"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
